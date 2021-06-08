3119db5d06d57882fe370e4179876c744c71edca
Vivian <yuweizhang88@gmail.com>
2021-06-08 15:57:47 +1000

    create app, contact, home page

---
+++ app_day1/app/controllers/homes_controller.rb
@@ -0,0 +1,12 @@
+class HomesController < ApplicationController
+
+    def index
+    end
+
+    def apps
+    end
+
+    def contacts
+    end
+
+end
--- app_day1/config/routes.rb
+++ app_day1/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "homes#index"
+  
+  get '/apps' =>'homes#apps'
+  get '/contacts' =>'homes#contacts'
+
 end
